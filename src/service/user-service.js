592f0dd25c109ee6377f838a326b926900bf110c
Syeda Umme Kulsum <syedaummekulsum9@gmail.com>
2025-08-26 20:55:38 +0530

    Implemented buiseness logic for registration in service layer

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,7 +1,30 @@
 console.log("user-service");
 
+const UserDao = require("../dao/user-dao.js");
+
 const UserService = {
-    
+    register:(payload)=>{
+        return new Promise(async(resolve, reject)=>{
+            let isUserExist=await UserDao.isUsernameExist(payload);
+            if(isUserExist)
+                 return reject("Username already exist. Please signin");
+            
+            UserDao.register(payload).then(result=>{
+                console.log("Returned data from dao to service",result)
+                resolve({
+                    message:"User registered successfully",
+                    data:{
+                        firstname:result.firstname,
+                        usermame:result.username,
+                        role:result.role,
+                        _id:result._id
+                    }
+                }).catch(error=>{
+                    reject(error)
+                });
+            }) ; 
+        });
+   }
     
 }
 
